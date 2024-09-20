7fe08aea87121346dc228df41b1ed4147c9eebe9
David Paulsson <david@davidpaulsson.se>
2024-09-20 09:06:23 +0200

    Init ykp.se

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,67 @@
 import Image from "next/image";
+import profilePicture from "./yaty.jpg";
+
+export const dynamicParams = false;
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const age = new Date().getFullYear() - 1987;
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+  return (
+    <div className="p-8 pb-20 flex justify-center min-h-screen items-center font-[family-name:var(--font-geist-sans)]">
+      <main className="max-w-prose space-y-6 m-auto ">
+        <h1 className="text-left w-full font-bold mb-8 text-xl">
+          Yaty Karim Paulsson
+        </h1>
+
+        <Image
+          src={profilePicture}
+          alt="Yaty Karim Paulsson"
+          width={201}
+          height={300}
+          className="rounded-sm md:float-right md:!mb-8 md:ml-8"
+        />
+        <h2 className="text-left w-full">Hej!</h2>
+        <p>Det är jag som är Yaty Karim Paulsson. Ni kan kalla mig Yaty!</p>
+        <p>
+          Jag jobbar som teamledare inom Learning and Development på Bravida.
+          Här jobbar jag med vår interna skola som erbjuder kompetensutveckling
+          för våra medarbetare. Jag ansvarar primärt för vår Sverige-verksamhet
+          men jobbar även med några av våra nordiska program. Bravida är ett
+          installationsföretag och har cirka 13000 medarbetare i Sverige, Norge,
+          Danmark och Finland.
+        </p>
+        <p>
+          Under mitt yrkesverksamma liv har jag haft lite olika roller där jag
+          samlat på mig flera års erfarenhet av projekt- och produktionsledning
+          inom kommunikation. Bland annat på varumärkesbyrån Revolution,
+          ViacomCBS (MTV, Comedy central, Nickelodeon) samt filmbyrån Prolounge.
+          Jag har också jobbat som HR/People manager på SEN Street Kitchen som
+          fokuserar på asiatisk street food. Mina fokusområden var rekrytering
+          av de ledande rollerna på restaurangerna samt onboarding, utbildning
+          och employer branding. Jag älskar att jobba med människor och brinner
+          för kompetensutveckling.
+        </p>
+        <p>
+          Är du intresserad av att veta mer om mig? Maila mig på{" "}
+          <a className="underline hover:no-underline" href="mailto:yaty@ykp.se">
+            yaty@ykp.se
+          </a>{" "}
+          och/eller connecta med mig på{" "}
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            className="underline hover:no-underline"
+            href="https://www.linkedin.com/in/yatypaulsson/"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            LinkedIn
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+          !
+        </p>
+        <p>
+          Jag är {age} år och bor med min man David och våra två barn i
+          stockholmsförort. Min vardag innefattar mycket kaffe, träning,
+          matlagning, trädgård, växter ock såklart familjehäng. Kollar tyvärr på
+          orimligt många reality-serier när jag får lite tid över.
+        </p>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
